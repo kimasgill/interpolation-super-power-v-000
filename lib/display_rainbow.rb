70725d5b38a4e2de755603d6013141dd04b8aec1
Kimberly Asgill <kimberlyasgill@gmail.com>
2018-11-09 04:19:48 +0000

    automatically backed up by learn

---
+++ lib/display_rainbow.rb
@@ -0,0 +1,3 @@
+#define a method called display_rainbow
+#display_rainbow must accept an argument, an array of colors.
+#display_rainbow should print the colors of the rainbow in the format provided
