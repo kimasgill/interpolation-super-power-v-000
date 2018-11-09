b2026b85b8e913b3cd50e4f3644c4a24e0e11946
Kimberly Asgill <kimberlyasgill@gmail.com>
2018-11-09 04:19:56 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,3 +1,3 @@
 #define a method called display_rainbow
 #display_rainbow must accept an argument, an array of colors.
-#display_rainbow should print the colors of the rainbow in the format provided
+#display_rainbow should print the colors of the rainbow in the format provided: "R: red, O: orange, Y: yellow, G: green, B: blue, I: indigo, V: violet"
